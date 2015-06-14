7b74040ba5475e35c8a88e1eda92f7f467f99843
Bhargav Anand <rjn143@gmail.com>
2015-06-13 21:27:23 -0400

    added server task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,17 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
+var server = require('gulp-webserver');
 var livereload = require('gulp-livereload');
+
+
+gulp.task('serve', function(){
+  return gulp.src('./')
+    .pipe(server({
+      livereload: true,
+      open: true,
+    }));
+});
 
 gulp.task('copy', function(){
   return gulp.src(['./pages/index.html', './pages/bg.png', './pages/yoda.png', './pages/screen.png'])
@@ -22,7 +32,7 @@ gulp.task('less', function(){
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['copy', 'icons', 'less'], function(){
+gulp.task('default', ['serve', 'copy', 'icons', 'less'], function(){
   gulp.watch('./pages/index.html', ['copy']);
   gulp.watch('./pages/index.less', ['less']);
 
